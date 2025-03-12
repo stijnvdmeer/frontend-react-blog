e0585bc2419d5b8beb22ee84222354432744d075
stijnvdmeer <stijn08072002@outlook.com>
2025-03-12 11:12:39 +0100

    posts page header

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,9 +1,10 @@
-
+import data from '../constants/data.json'
 
 export default function Posts() {
+    console.log(data)
     return (
         <div>
-            Post Overview!
+            <h1>Bekijk alle {data.length} posts op het platform</h1>
         </div>
     )
 }
