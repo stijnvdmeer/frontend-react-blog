2ca3f37217efeec441ddb4f8f71f7072f6cce4b9
stijnvdmeer <stijn08072002@outlook.com>
2025-03-12 11:23:08 +0100

    added classes to PostOverview component

--- src/components/postoverview/PostOverview.jsx
+++ src/components/postoverview/PostOverview.jsx
@@ -4,7 +4,7 @@ export default function PostOverview({title, author, reactions, shares}) {
     return (
         <article className="postoverview">
             <p>
-                <span>{title}</span>
+                <span className="title">{title}</span>
                 ({author})
             </p>
             <p>{reactions} reacties - {shares} keer gedeeld</p>
