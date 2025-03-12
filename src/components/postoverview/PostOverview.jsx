350fc413eda9ef13a62b472193d49d99d83a4986
stijnvdmeer <stijn08072002@outlook.com>
2025-03-12 11:19:36 +0100

    Created PostOverview component

---
+++ src/components/postoverview/PostOverview.jsx
@@ -0,0 +1,13 @@
+import './postoverview.css';
+
+export default function PostOverview({title, author, reactions, shares}) {
+    return (
+        <article className="postoverview">
+            <p>
+                <span>{title}</span>
+                ({author})
+            </p>
+            <p>{reactions} reacties - {shares} keer gedeeld</p>
+        </article>
+    )
+}
