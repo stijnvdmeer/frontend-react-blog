6799656cec7a9d58f52ab9c814e9d1e5de65f22f
stijnvdmeer <stijn08072002@outlook.com>
2025-03-12 16:26:35 +0100

    get data from props

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,7 +1,7 @@
-import data from '../constants/data.json';
+
 import PostOverview from "../components/postoverview/PostOverview.jsx";
 
-export default function Posts() {
+export default function Posts({data}) {
     return (
         <div>
             <h1>Bekijk alle {data.length} posts op het platform</h1>
