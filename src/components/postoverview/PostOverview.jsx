a09109300bb73cd94d14cb61f4d342ba452ac700
stijnvdmeer <stijn08072002@outlook.com>
2025-03-12 11:31:34 +0100

    added link encompassing PostOverview Component

--- src/components/postoverview/PostOverview.jsx
+++ src/components/postoverview/PostOverview.jsx
@@ -1,13 +1,21 @@
 import './postoverview.css';
 
-export default function PostOverview({title, author, reactions, shares}) {
+import {Link} from 'react-router-dom'
+export default function PostOverview({id, title, author, reactions, shares}) {
     return (
-        <article className="postoverview">
-            <p>
-                <span className="title">{title}</span>
-                ({author})
-            </p>
-            <p>{reactions} reacties - {shares} keer gedeeld</p>
-        </article>
+        <Link
+            to={`/postdetails/${id}`}
+            className="link"
+        >
+            <article className="postoverview">
+
+                    <p>
+                        <span className="title">{title}</span>
+                        ({author})
+                    </p>
+                    <p>{reactions} reacties - {shares} keer gedeeld</p>
+
+            </article>
+        </Link>
     )
 }
