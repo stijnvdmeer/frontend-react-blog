7b481dd671c0412875e53f5fc2575e4be66f7dd2
stijnvdmeer <stijn08072002@outlook.com>
2025-03-12 14:13:33 +0100

    implemented formatDateTime helper function

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -1,17 +1,17 @@
 import '../styles/postdetails.css';
 import {Link, useParams} from 'react-router-dom';
 import data from '../constants/data.json';
+import formatDateTime from '../helperfunctions/formatDateTime';
 
 export default function PostDetails() {
     const { id } = useParams();
     const post = data[id];
 
-    console.log(post);
     return (
         <div className="post-details">
             <h1>{post.title}</h1>
             <h3>{post.subtitle}</h3>
-            <span>Geschreven door {post.author} op {post.created}</span>
+            <span>Geschreven door {post.author} op {formatDateTime(post.created)}</span>
             <span className="readtime"> &#9202; {post.readTime} minuten lezen</span>
             <p>{post.content}</p>
 
