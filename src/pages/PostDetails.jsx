4d778274f5a6019ed361f9495b385ab4d4420d54
stijnvdmeer <stijn08072002@outlook.com>
2025-03-12 11:49:01 +0100

    created basic structure for PostDetails page

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -1,11 +1,20 @@
-import { useParams } from 'react-router-dom'
+import {Link, useParams} from 'react-router-dom';
+import data from '../constants/data.json';
 
 export default function PostDetails() {
     const { id } = useParams();
+    const post = data[id];
 
+    console.log(post);
     return (
         <div>
-            {id}
+            <h1>{post.title}</h1>
+            <h3>{post.subtitle}</h3>
+            <span>Geschreven door {post.author} op {post.created}</span>
+            <p>{post.content}</p>
+
+            <span>{post.comments} reacties - {post.shares} keer gedeeld</span>
+            <Link to="/posts">Terug naar overzichtspagina</Link>
         </div>
     )
 }
