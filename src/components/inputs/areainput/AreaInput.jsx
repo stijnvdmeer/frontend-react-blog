ad8aa0a040eb0c37f1ebb2c31354c53bd432144c
stijnvdmeer <stijn08072002@outlook.com>
2025-03-12 15:26:25 +0100

    added onchange handler to input fields

--- src/components/inputs/areainput/AreaInput.jsx
+++ src/components/inputs/areainput/AreaInput.jsx
@@ -1,10 +1,10 @@
 import "./areainput.css";
 
-export default function AreaInput({fieldName, fieldText}) {
+export default function AreaInput({fieldName, fieldText, fieldValue, onChangeFunc}) {
     return (
         <div className="areainput">
             <label htmlFor={fieldName}>{fieldText + ":"}</label>
-            <textarea id={fieldName} />
+            <textarea id={fieldName} value={fieldValue} onChange={(e) => onChangeFunc(e.target.value, fieldName)}/>
         </div>
     )
 }
--- src/components/inputs/textinput/TextInput.jsx
+++ src/components/inputs/textinput/TextInput.jsx
@@ -1,10 +1,11 @@
 import "./textinput.css";
 
-export default function TextInput({fieldName, fieldText}) {
+export default function TextInput({fieldName, fieldText, fieldValue, onChangeFunc}) {
+
     return (
         <div className="textinput">
             <label htmlFor={fieldName}>{fieldText + ":"}</label>
-            <input type="text" id={fieldName} />
+            <input type="text" id={fieldName} value={fieldValue} onChange={(e) => onChangeFunc(e.target.value, fieldName)} />
         </div>
     )
 }
