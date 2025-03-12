a99b4e529e9491ef75f23021b8a292de0ef9bb63
stijnvdmeer <stijn08072002@outlook.com>
2025-03-12 16:26:11 +0100

    created data usestate in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,26 @@ import CreatePost from "./pages/CreatePost.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import PostDetails from "./pages/PostDetails.jsx";
 import NavBar from "./components/navbar/NavBar.jsx";
+import {useState} from "react";
+
+import list from "./constants/data.json";
 
 
 function App() {
+    const [data, setData] = useState(list);
+
+    function changeData(newData) {
+        setData(newData);
+    }
+
     return (
         <div className="page-container">
             <NavBar className="navigation" />
             <Routes>
                 <Route path="/" element={<Home/>} />
-                <Route path="/posts" element={<Posts />} />
-                <Route path="/createpost" element={<CreatePost />} />
-                <Route path="/postdetails/:id" element={<PostDetails />} />
+                <Route path="/posts" element={<Posts data={data}/>} />
+                <Route path="/createpost" element={<CreatePost data={data} changeDataFunction={changeData}/>} />
+                <Route path="/postdetails/:id" element={<PostDetails data={data}/>} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
