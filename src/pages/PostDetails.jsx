fd004b2767da1504814ecfddcc8d5e72a3e2ccc1
stijnvdmeer <stijn08072002@outlook.com>
2025-03-12 13:55:56 +0100

    added classes to give styling

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -1,3 +1,4 @@
+import '../styles/postdetails.css';
 import {Link, useParams} from 'react-router-dom';
 import data from '../constants/data.json';
 
@@ -7,14 +8,20 @@ export default function PostDetails() {
 
     console.log(post);
     return (
-        <div>
+        <div className="post-details">
             <h1>{post.title}</h1>
             <h3>{post.subtitle}</h3>
             <span>Geschreven door {post.author} op {post.created}</span>
+            <span className="readtime"> &#9202; {post.readTime} minuten lezen</span>
             <p>{post.content}</p>
 
             <span>{post.comments} reacties - {post.shares} keer gedeeld</span>
-            <Link to="/posts">Terug naar overzichtspagina</Link>
+            <Link
+                to="/posts"
+                className="post-details__link"
+            >
+                &lArr;  Terug naar overzichtspagina
+            </Link>
         </div>
     )
 }
