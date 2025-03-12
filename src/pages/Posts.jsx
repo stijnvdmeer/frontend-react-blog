a629d7118303029ca2f151ddea60e98db531a125
stijnvdmeer <stijn08072002@outlook.com>
2025-03-12 11:25:21 +0100

    added data map to print PostOverview for each object in data

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,10 +1,22 @@
 import data from '../constants/data.json'
+import PostOverview from "../components/postoverview/PostOverview.jsx";
 
 export default function Posts() {
-    console.log(data)
+    const testData = data[1];
     return (
         <div>
             <h1>Bekijk alle {data.length} posts op het platform</h1>
+            {
+                data.map(post => (
+                    <PostOverview
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        reactions={post.reactions}
+                        shares={post.shares}
+                    />
+                ))
+            }
         </div>
     )
 }
